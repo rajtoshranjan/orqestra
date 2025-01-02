fba16a95f55d8d0f67acb89face7be45065e0860
Rajtosh Ranjan <rajtoshranjan@gmail.com>
2025-01-02 11:18:32 +0530

    Setup shadcn

--- ui/src/header/header.tsx
+++ ui/src/header/header.tsx
@@ -1,9 +1,33 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
 import React from "react";
 
 export const Header: React.FC = () => {
   return (
-    <header className="w-full h-12 border-b border-gray-200 bg-black opacity-85 fixed z-50 flex items-center px-5 shadow-lg">
-      <h3 className="text-white text-lg font-bold">DrawOps</h3>
-    </header>
+    <Menubar className="w-full h-12 fixed z-50 px-5 opacity-80 shadow-lg border-l-0 border-r-0 border-t-0 rounded-none border-border bg-background">
+      <MenubarMenu>
+        <MenubarTrigger className="text-primary text-lg font-bold">
+          DrawOps
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>Home</MenubarItem>
+          <MenubarItem>
+            New Project <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Logout</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
   );
 };
